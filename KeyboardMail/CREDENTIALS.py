077702fd32b95703e34be213387d18b8df15f34b
mateosalta <mateo_salta@yahoo.com>
2015-10-28 09:32:20 -0500

    Emptying out the JunkBin

--- KeyboardMail/CREDENTIALS.py
+++
@@ -1,4 +0,0 @@
-# Currently, the only site supported is GMail
-
-USERNAME = 'keyboardmailtesting@gmail.com' 	# Replace with your own username
-PASSWORD = 'k3yb0ardMa!l'			# Replace with your own password
